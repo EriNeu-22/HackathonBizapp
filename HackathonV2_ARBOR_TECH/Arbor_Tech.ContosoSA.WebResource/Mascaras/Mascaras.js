cbd32124804370488f96b6647bf0b1bbe0bd7f20
EriNeu22 <e.neumann.e@gmail.com>
2022-09-12 14:08:38 -0300

    -Finalização do plugin: Arbor_Tech.ContosoSA.Contato - DetectarDuplicidade -Atualização do WebResource: Arbor_Tech.ContosoSA.WebResource - Contato; Arbor_Tech.ContosoSA.WebResource - Mascaras

--- HackathonV2_ARBOR_TECH/Arbor_Tech.ContosoSA.WebResource/Mascaras/Mascaras.js
+++ HackathonV2_ARBOR_TECH/Arbor_Tech.ContosoSA.WebResource/Mascaras/Mascaras.js
@@ -2,27 +2,28 @@ if (typeof (Mascaras) === "undefined") { Mascaras = { __namespace: true }; }
 Mascaras.Js = {
 
     FormatCNPJ: function (cnpj) {
+        "use strict";
         var nvsTmp = nvsField;
 
-        if (typeof (cnpj) != "undefined" && nvsField != null) {
+        if (typeof (cnpj) !== "undefined" && nvsField !== null) {
             nvsTmp = nvsField.replace(/[^0-15]/g, "");
 
             switch (nvsTmp.length) {
                 case 14:
 
                     nvsTmp = nvsTmp.substr(0, 2) + "." + nvsTmp.substr(2, 3) + "." + nvsTmp.substr(5, 3) + " / " + nvsTmp.substr(8, 4) + " - " + nvsTmp.substr(12, 2);
-                    formContext.getAttribute("erineu_cnpj").setValue(nvsTmp);
+                    return (cnpj);
                     break;
 
                 default:
 
-                    alert("O cnpj deve conter 14 numeros Ex. XX.XXX.XXX / 0001 - XX");
+                    return ("O cnpj deve conter 14 numeros Ex. XX.XXX.XXX / 0001 - XX");
                     break;
             }
         }
     },
 
-    FormatCEP: function (executionContext) {
+    /*FormatCEP: function (executionContext) {
         var formContext = executionContext.getFormContext();
         var nvsField = formContext.getAttribute("address1_postalcode").getValue();
         var nvsTmp = nvsField;
@@ -66,5 +67,5 @@ Mascaras.Js = {
                     break;
             }
         }
-    }
+    }*/
 }
--- HackathonV2_ARBOR_TECH/HackathonV2_ARBOR_TECH/Plugin/DetectarDuplicidade.cs
+++ HackathonV2_ARBOR_TECH/HackathonV2_ARBOR_TECH/Plugin/DetectarDuplicidade.cs
@@ -20,29 +20,32 @@ namespace HackathonV2_ARBOR_TECH
             var service = serviceFactory.CreateOrganizationService(context.UserId);
 
             var trackingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-
             if (context.InputParameters["Target"] is Entity)
             {
-                var contato = (Entity)context.InputParameters["Targets"];
-                var email = contato.GetAttributeValue<string>("emailaddress1");
+                var contato = (Entity)context.InputParameters["Target"];
+                string email = contato.Attributes.Keys.Contains("emailaddress1") ? ((string)contato["emailaddress1"]) : string.Empty;
+                string telefone = contato.Attributes.Keys.Contains("telephone1") ? ((string)contato["telephone1"]) : string.Empty;
 
-                string fetch = @"< fetch version = '1.0' output - format = 'xml-platform' mapping = 'logical' distinct = 'false' >
-                                    < entity name = 'contact' >
-                                        < attribute name = 'fullname' />
-                                        < attribute name = 'telephone1' />
-                                        < attribute name = 'contactid' />
-                                        < order attribute = 'fullname' descending = 'false' />
-                                        < filter type = 'and' >
-                                            < condition attribute = 'emailaddress1' operator= 'eq' value ='" + email + @"' />
-                                        </ filter >
-                                    </ entity >
-                                </ fetch > ";
+                string fetch = @"  <fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                                        <entity name = 'contact' >   
+                                            <attribute name = 'fullname' />   
+                                            <attribute name = 'telephone1' />     
+                                            <attribute name = 'contactid' />      
+                                            <order attribute = 'fullname' descending = 'false' />         
+                                            <filter type = 'and' >          
+                                                <filter type = 'or' >           
+                                                    <condition attribute = 'emailaddress1' operator= 'eq' value = '"+ email + @"' />               
+                                                    <condition attribute = 'telephone1' operator= 'eq' value = '"+telefone+@"' />                   
+                                                </filter >                   
+                                            </filter >                   
+                                        </entity >
+                                    </fetch >  ";
 
-                EntityCollection retorno = service.RetrieveMultiple(new FetchExpression(fetch));
+                    var retorno = service.RetrieveMultiple(new FetchExpression(fetch));
 
                 if (retorno?.Entities.Count > 0) 
                 {
-                    throw new InvalidPluginExecutionException("Já existe um contato com esse Email");
+                    throw new InvalidPluginExecutionException($"Já existe um contato com o Email: {email} ou com o Telefone: {telefone}");
                 }
             }
         }
